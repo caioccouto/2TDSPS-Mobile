a12bb042ba8070d159edbe64bee844dff40e23f8
Caio Couto <caiocc20006@gmail.com>
2026-08-17 20:37:52 -0300

    Criação de variáveis

--- OlaMundo.dart
+++ OlaMundo.dart
@@ -1,7 +1,22 @@
-//Comentário de Linha
+//
 
 /*
-Comentario
+cuzin
 de 
-Bloco
+crente
 */
+
+void main(){
+
+    String nome = 'Caio';
+    int idade = 837;
+    String curso = 'Gastronomia';
+    double nota = 3.5;
+    bool matriculado = true; 
+
+    print('Nome: $nome');
+    print('Idade: $idade');
+    print('Curso: $curso');
+    print('Nota: $nota');
+    print('Matriculado: $matriculado');
+}
